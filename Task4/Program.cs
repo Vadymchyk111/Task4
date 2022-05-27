7da347c869a8275790d892ff0a9968b04492a8b1
Вадим Павличенко <Vadymchyk111@gmail.com>
2022-05-27 13:24:14 +0300

    AddCode

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -10,6 +10,17 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            string name;
+            string zodiacSign;
+            string age;
+
+            Console.WriteLine("Как вас зовут?");
+            name = Console.ReadLine();
+            Console.WriteLine("Какой ваш знак Зодиака?");
+            zodiacSign = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            age = Console.ReadLine();
+            Console.WriteLine("Вас зовут - "+name+", вам "+age+" лет, ваш знак зодиака - "+zodiacSign);
         }
     }
 }
